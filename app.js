7b5956aa19076522d68bff2f4b9a9b8ac9dd92fd
Erol Esen <ErolEsen.Personal@gmail.com>
2024-11-07 18:29:20 -0500

    added ng click event handler

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
+// Make sure none of the variables/objects/functions "leak to the global scope."
+// Using Immediately Invoked Function Expression (IIFE)
 (function () {
 'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Yaakov";
-  $scope.stateOfBeing = "hungry";
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.myMessage = "";
 
-  $scope.sayMessage = function () {
-    return "Yaakov likes to eat healthy snacks at night!";
-  };
-
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
+  $scope.clickCheckIfTooMuch = function () {
+    $scope.myMessage = "Hello, World!";
+  }
 }
 
 })();
